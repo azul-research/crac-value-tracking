92904f6c03ac4f12e650dd64d15d9a11ebe1fb34
Дарья Суворова <dariasuvorova@Daras-MacBook-Pro.local>
2024-06-19 13:24:11 +0300

    example with if added

---
+++ src/main/java/example2/Main.java
@@ -0,0 +1,15 @@
+package example2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 0;
+        String b = args[1];
+        String c = "";
+        if (b == null) {
+            a = 2;
+            c = args[1];
+        }
+
+    }
+}
