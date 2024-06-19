1b2214728c54c0faa3507374f27e46ad605cfe39
Дарья Суворова <dariasuvorova@Daras-MacBook-Pro.local>
2024-06-19 21:14:29 +0300

    test

--- src/test/java/analysis/ClassFileanalyserTest.java
+++ src/test/java/analysis/ClassFileanalyserTest.java
@@ -3,7 +3,10 @@ package analysis;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import static input.JarBytecodeExtractor.analyseJarFile;
+import static org.junit.Assert.assertEquals;
 
 public class ClassFileanalyserTest {
 
@@ -13,6 +16,7 @@ public class ClassFileanalyserTest {
 
         var result = analyseJarFile("src/test/java/examples/simpleif/simpleif.jar");
         System.out.println(result);
+        assertEquals(result, Set.of(0, 1));
 
     }
 
@@ -21,5 +25,23 @@ public class ClassFileanalyserTest {
         var result = analyseJarFile("src/test/java/examples/simpleifelse/simpleifelse.jar");
         System.out.println(result);
 
+        assertEquals(result, Set.of(0, 2, 3));
+    }
+
+    @Test
+    public void nestedIfTest() {
+        var result = analyseJarFile("src/test/java/examples/nestedif/nestedif.jar");
+        System.out.println(result);
+
+        assertEquals(result, Set.of(0, 1, 4));
+    }
+
+
+    @Test
+    public void nestedIf2Test() {
+        var result = analyseJarFile("src/test/java/examples/nestedif2/nestedif2.jar");
+        System.out.println(result);
+
+        assertEquals(result, Set.of(0, 1, 3, 4));
     }
 }
