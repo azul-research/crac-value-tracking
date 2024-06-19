a6edd2e627df05d547d6137fab6c236519bf874e
Дарья Суворова <dariasuvorova@Daras-MacBook-Pro.local>
2024-06-19 17:32:17 +0300

    script for creating examples

---
+++ script.sh
@@ -0,0 +1,2 @@
+javac /Users/dariasuvorova/IdeaProjects/unsafecodeanalysis/src/test/java/examples/$1/Main.java
+jar cf /Users/dariasuvorova/IdeaProjects/unsafecodeanalysis/src/test/java/examples/$1/$1.jar /Users/dariasuvorova/IdeaProjects/unsafecodeanalysis/src/test/java/examples/$1/Main.class
